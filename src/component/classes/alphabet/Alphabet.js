9c27f2ac7c9593723b4e60a4798d705d2f15d2ba
masooma zahedi <masooma.zahedi1@gmail.com>
2024-12-26 10:13:59 -0600

    ok

--- src/component/classes/alphabet/Alphabet.js
+++ src/component/classes/alphabet/Alphabet.js
@@ -8,11 +8,12 @@ export const Alphabet = () => {
   return (
     <>
         <div className = "container border border-warning mt-4">
-        <MakeWordCarousel carouselName={testColor.wordCarousel} carouselData={testColor.alphaColor}/>
-        <MakeWordCarousel carouselName={testFruit.wordCarousel} carouselData={testFruit.alphaImage}/>
-        <MakeWordCarousel carouselName={testBody.wordCarousel} carouselData={testBody.alphaBody}/>
-        <MakeWordCarousel carouselName={testWear.wordCarousel} carouselData={testWear.alphaWear}/>
+          <MakeWordCarousel carouselName={testColor.wordCarousel} carouselData={testColor.alphaColor}/>
+          <MakeWordCarousel carouselName={testFruit.wordCarousel} carouselData={testFruit.alphaImage}/>
+          <MakeWordCarousel carouselName={testBody.wordCarousel} carouselData={testBody.alphaBody}/>
+          <MakeWordCarousel carouselName={testWear.wordCarousel} carouselData={testWear.alphaWear}/>
         </div>
     </>
   )
 }
+ 
