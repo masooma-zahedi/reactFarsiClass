de102c083d7b8b0e308607db086c6c7302b86fdd
masooma zahedi <masooma.zahedi1@gmail.com>
2024-12-26 11:12:15 -0600

    make 3 data for card alphabet card

---
+++ src/component/classes/alphabet/cardAlpha/cardDataAlpha.js
@@ -0,0 +1,23 @@
+export let infoCard = [
+    {
+        mainPic:"./images/assetAlpha/cardAlpha/آ--04-10.jpg",
+        title:"حرف الف",
+        examplePic:["./images/assetAlpha/cardAlpha/exampleAlpha/08-10-الف-1.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-الف-2.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-الف-3.jpg"],
+        sortSoundPic:"./images/assetAlpha/cardAlpha/اَ-03-10-3.jpg",
+        longSoundPic:"./images/assetAlpha/cardAlpha/06-10-24-الف-long.jpg"
+    },
+    {
+        mainPic:"./images/assetAlpha/cardAlpha/ب-03-10-1.jpg",
+        title:"حرف ب",
+        examplePic:["./images/assetAlpha/cardAlpha/exampleAlpha/08-10-ب-1.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-ب-1.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-ب-1.jpg"],
+        sortSoundPic:"./images/assetAlpha/cardAlpha/ب-03-10-3.jpg",
+        longSoundPic:"./images/assetAlpha/cardAlpha/06-10-ب-long.jpg"
+    },
+    {
+        mainPic:"./images/assetAlpha/cardAlpha/03-10-پ-1.jpg",
+        title:"حرف پ",
+        examplePic:["./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-1.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-1.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-1.jpg",],
+        sortSoundPic:"./images/assetAlpha/cardAlpha/3-10-03-پ.jpg",
+        longSoundPic:"./images/assetAlpha/cardAlpha/06-10-24-پ-long.jpg"
+    },
+]
