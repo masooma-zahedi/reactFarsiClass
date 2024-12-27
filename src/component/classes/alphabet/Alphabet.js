d173bd722da934ceb18114e2e85e8233cc7031a8
masooma zahedi <masooma.zahedi1@gmail.com>
2024-12-27 13:23:44 -0600

    add exalple pic to alphabet letter instructuer

--- src/component/classes/alphabet/Alphabet.js
+++ src/component/classes/alphabet/Alphabet.js
@@ -1,6 +1,6 @@
 import { testBody, testColor, testFruit, testWear } from "./dataAlpha.js"
-
 import { MakeWordCarousel } from "./MakeWordCarousel.js";
+import {CardAlphaCompo} from "./cardAlpha/CardAlphaCompo.js"
 
 
 export const Alphabet = () => {
@@ -8,6 +8,7 @@ export const Alphabet = () => {
   return (
     <>
         <div className = "container border border-warning mt-4">
+          <CardAlphaCompo/>
           <MakeWordCarousel carouselName={testColor.wordCarousel} carouselData={testColor.alphaColor}/>
           <MakeWordCarousel carouselName={testFruit.wordCarousel} carouselData={testFruit.alphaImage}/>
           <MakeWordCarousel carouselName={testBody.wordCarousel} carouselData={testBody.alphaBody}/>
---
+++ src/component/classes/alphabet/cardAlpha/CardAlphaCompo.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {infoCard} from './cardDataAlpha';
+
+export const CardAlphaCompo = () => {
+    // console.log(infoCard[0].title);
+    
+  return (
+    <>
+        <section className='f-right mt-5'>
+            <div className="row" >
+                {/* *** start Num 1 *** */}
+                {
+                    infoCard.map((letter,index)=>{
+                        // console.log(index);
+                        return(
+                            <>
+                                <div className="col-md-3" key={index}>
+                                    <div className="card" style={{width: "18rem"}}>
+                                        <img src={letter.mainPic} className="card-img-top" data-bs-toggle="modal" data-bs-target={`#${letter.examplePicId}`}  alt="حرف آ" />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{letter.title}</h5>
+                                            <p className="card-text">همراه با صداهای کوتاه و کشیده</p>
+                                            <a href="#" className="btn btn-primary ms-1" data-bs-toggle="modal" data-bs-target="#letterA-1">صداهای کوتاه</a>
+                                            <a href="#" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#letterA-2">صداهای کشیده</a>
+                                        </div>
+                                    </div> 
+                                    {/*  stsrt modal - Example Letter  */}
+                                    <div className="modal fade" style={{backgroundColor:' rgba(0, 0, 0, 0.619)'}} id={letter.examplePicId}  data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div className="modal-dialog modal-dialog-centered border border-light" style={{width: '700px'}}>
+                                        <div className="modal-content bg-danger"  style={{width: '700px', minHeight:'500px'}}>
+                                            <div className="modal-header">
+                                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            <div className="modal-title" style={{maxiWdth: '300px', maxWidth:' 100px'}} id="staticBackdropLabel">
+                                                <img src="./images/short-sound.jpg" className="w-100 h-100" alt="short-sound" srcSet="" />
+                                            </div>
+                                            </div>
+                                            <div className="modal-body">
+                                                {
+                                                    letter.examplePic.map((exaPic,index)=>{
+                                                        return(
+                                                            <>
+                                                                <div style={{height: '400px'}} key={index} className="mb-3">
+                                                                    <img src={exaPic} className="w-100 h-100" alt="" srcSet=""/>
+                                                                </div>
+                                                            </>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+                                            <div className="modal-footer">
+                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="button" className="btn btn-primary">Understoodssss</button>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    {/* end modal-- Example Letter  */}
+
+
+
+
+
+
+
+
+                                </div>
+                            </>
+                        )
+                        
+                    })
+                }
+                {/* **** Start Num 2 **** */}
+                {/* <div className="col-md-3">    
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src="./images/آ--04-10.jpg" className="card-img-top" data-bs-toggle="modal" data-bs-target="#exampleL-A1"  alt="حرف آ" />
+                        <div className="card-body">
+                            <h5 className="card-title">حرف الف</h5>
+                            <p className="card-text">همراه با صداهای کوتاه و کشیده</p>
+                            <a href="#" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#letterA-1">صداهای کوتاه</a>
+                            <a href="#" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#letterA-2">صداهای کشیده</a>
+                        </div>
+                    </div>     
+                </div> */}
+                {/* **** End Num 2 **** */}
+            </div>
+        </section>
+
+    </>
+  )
+}
