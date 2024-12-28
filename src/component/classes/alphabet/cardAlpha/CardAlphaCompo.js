852eace2198976e5373333cb2a1fcdd09701809e
masooma zahedi <masooma.zahedi1@gmail.com>
2024-12-28 10:14:48 -0600

    make shortSound pic at alphbet part

--- src/component/classes/alphabet/cardAlpha/CardAlphaCompo.js
+++ src/component/classes/alphabet/cardAlpha/CardAlphaCompo.js
@@ -15,12 +15,12 @@ export const CardAlphaCompo = () => {
                         return(
                             <>
                                 <div className="col-md-3" key={index}>
-                                    <div className="card" style={{width: "18rem"}}>
+                                    <div className="card" style={{width:"18rem"}}>
                                         <img src={letter.mainPic} className="card-img-top" data-bs-toggle="modal" data-bs-target={`#${letter.examplePicId}`}  alt="حرف آ" />
                                         <div className="card-body">
                                             <h5 className="card-title">{letter.title}</h5>
                                             <p className="card-text">همراه با صداهای کوتاه و کشیده</p>
-                                            <a href="#" className="btn btn-primary ms-1" data-bs-toggle="modal" data-bs-target="#letterA-1">صداهای کوتاه</a>
+                                            <a href="#" className="btn btn-primary ms-1" data-bs-toggle="modal" data-bs-target={`#${letter.shortSoundId}`}>صداهای کوتاه</a>
                                             <a href="#" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#letterA-2">صداهای کشیده</a>
                                         </div>
                                     </div> 
@@ -56,6 +56,32 @@ export const CardAlphaCompo = () => {
                                     </div>
                                     {/* end modal-- Example Letter  */}
 
+
+
+
+                                    {/* <!-- stsrt modal - Short Sound --> */}
+                                    <div className="modal fade" id={letter.shortSoundId} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div className="modal-dialog modal-dialog-centered">
+                                        <div className="modal-content" style={{width: '700px', height: '500px'}}>
+                                            <div className="modal-header">
+                                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            <div className="modal-title" style={{maxWidth: '300px', maxWidth: '100px'}} id="staticBackdropLabel">
+                                                <img src="./images/short-sound.jpg" className="w-100 h-100" alt="short-sound" srcSet="" />
+                                            </div>
+                                            </div>
+                                            <div className="modal-body">
+                                            <div>
+                                                <img src={letter.shortSoundPic} className="w-100 h-100" alt="" srcSet="" />  
+                                            </div>    
+                                            </div>
+                                            <div className="modal-footer">
+                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="button" className="btn btn-primary">Understood</button>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+            {/* <!-- end modal --> */}
 
 
 
--- src/component/classes/alphabet/cardAlpha/cardDataAlpha.js
+++ src/component/classes/alphabet/cardAlpha/cardDataAlpha.js
@@ -4,7 +4,8 @@ export let infoCard = [
         title:"حرف الف",
         examplePic:["./images/assetAlpha/cardAlpha/exampleAlpha/08-10-الف-1.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-الف-2.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-الف-3.jpg"],
         examplePicId:"exampleL-A1",
-        sortSoundPic:"./images/assetAlpha/cardAlpha/اَ-03-10-3.jpg",
+        shortSoundPic:"./images/assetAlpha/cardAlpha/اَ-03-10-3.jpg",
+        shortSoundId:"letterA-1",
         longSoundPic:"./images/assetAlpha/cardAlpha/06-10-24-الف-long.jpg"
     },
     {
@@ -12,7 +13,8 @@ export let infoCard = [
         title:"حرف ب",
         examplePic:["./images/assetAlpha/cardAlpha/exampleAlpha/08-10-ب-1.png","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-ب-2.png","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-ب-3.png"],
         examplePicId:"exampleL-B1",
-        sortSoundPic:"./images/assetAlpha/cardAlpha/ب-03-10-3.jpg",
+        shortSoundPic:"./images/assetAlpha/cardAlpha/ب-03-10-3.jpg",
+        shortSoundId:"de",
         longSoundPic:"./images/assetAlpha/cardAlpha/06-10-ب-long.jpg"
     },
     {
@@ -20,7 +22,8 @@ export let infoCard = [
         title:"حرف پ",
         examplePic:["./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-1.png","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-2.png","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-3.png","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-4.png",],
         examplePicId:"exampleL-P1",
-        sortSoundPic:"./images/assetAlpha/cardAlpha/3-10-03-پ.jpg",
+        shortSoundPic:"./images/assetAlpha/cardAlpha/3-10-03-پ.jpg",
+        shortSoundId:"sw",
         longSoundPic:"./images/assetAlpha/cardAlpha/06-10-24-پ-long.jpg"
     },
     {
@@ -28,7 +31,8 @@ export let infoCard = [
         title:"حرف پ",
         examplePic:["./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-1.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-1.jpg","./images/assetAlpha/cardAlpha/exampleAlpha/08-10-پ-1.jpg",],
         examplePicId:"exampleL-A1",
-        sortSoundPic:"./images/assetAlpha/cardAlpha/3-10-03-پ.jpg",
+        shortSoundPic:"./images/assetAlpha/cardAlpha/3-10-03-پ.jpg",
+        shortSoundId:"qw",
         longSoundPic:"./images/assetAlpha/cardAlpha/06-10-24-پ-long.jpg"
     },
 ]
