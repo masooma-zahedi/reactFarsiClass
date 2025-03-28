d6ce8dbd3221d2c01d5a4dc86e3676a2dd197ffd
masooma zahedi <masooma.zahedi1@gmail.com>
2025-03-28 10:36:47 -0500

    ok

--- src/component/classes/alphabet/Alphabet.js
+++ src/component/classes/alphabet/Alphabet.js
@@ -27,23 +27,23 @@ export const Alphabet = () => {
               <CarouselAlpha dataCarousel={alphaCrousel}/>
                {/* <CardAlphaCompo/>  */}
               <Routes>
-                                <Route path='/' element={<CardAlphaCompo/>}  />
-                                <Route path='/shortSound' element={<CarouselAlpha idCarouselAlpha="shorthref1"  dataCarousel={alphaShortCrousel}/>}  />
-                                <Route path='/longSound' element={<CarouselAlpha  idCarouselAlpha="longhref1"  dataCarousel={alphaLongCrousel}/>}  />
-                                <Route path='/storyA1' element={<CarouselAlpha  idCarouselAlpha="storyAl1"  dataCarousel={storyAlpha1}/>}  />
-                                <Route path='/storyA2' element={<CarouselAlpha  idCarouselAlpha="storyAl2"  dataCarousel={storyAlpha2}/>}  />
-                                <Route path='/storyA3' element={<CarouselAlpha  idCarouselAlpha="storyAl3"  dataCarousel={storyAlpha3}/>}  />
-                                <Route path='/read-1' element={<SylabeWords infoSentence={sylabelWord1}/>}  />
-                                <Route path='/read-2' element={<SylabeWords infoSentence={sylabelWord2}/>}  />
-                                <Route path='/read-3' element={<SylabeWords infoSentence={sylabelWord3}/>}  />
-                                <Route path='/read-4' element={<SylabeWords infoSentence={sylabelWord4}/>}  />
-                                <Route path='/read-5' element={<SylabeWords infoSentence={sylabelWord5}/>}  />
-                                <Route path='/numberGame' element={<FlippingTable idTablePlay="play1" tableData={tableDataNum.photoData} bgTable={tableDataNum.bgTable} showBack1={false} />}  />
-                                <Route path='/kitchenGame' element={<FlippingTable idTablePlay="kitU1" tableData={kitchenDataTable.photoData} bgTable={kitchenDataTable.bgTable} showBack1={true} />}  />
-                                <Route path='/professions' element={<FlippingTable idTablePlay="prof1" tableData={tableProfessions.photoData} bgTable={tableProfessions.bgTable} showBack1={false} />}  />
-                                <Route path='/animals' element={<FlippingTable idTablePlay="anim1" tableData={tableAnimals.photoData} bgTable={tableAnimals.bgTable} showBack1={true} />}  />
-                                <Route path='/tableColor_body' element={<FlippingTable idTablePlay="colorBo1" tableData={tableColor_body.photoData} bgTable={tableColor_body.bgTable} showBack1={true} />}  />
-                                <Route path='/wordTable/*' element={<WordTable />}  />
+                <Route path='/' element={<CardAlphaCompo/>}  />
+                <Route path='/shortSound' element={<CarouselAlpha idCarouselAlpha="shorthref1"  dataCarousel={alphaShortCrousel}/>}  />
+                <Route path='/longSound' element={<CarouselAlpha  idCarouselAlpha="longhref1"  dataCarousel={alphaLongCrousel}/>}  />
+                <Route path='/storyA1' element={<CarouselAlpha  idCarouselAlpha="storyAl1"  dataCarousel={storyAlpha1}/>}  />
+                <Route path='/storyA2' element={<CarouselAlpha  idCarouselAlpha="storyAl2"  dataCarousel={storyAlpha2}/>}  />
+                <Route path='/storyA3' element={<CarouselAlpha  idCarouselAlpha="storyAl3"  dataCarousel={storyAlpha3}/>}  />
+                <Route path='/read-1' element={<SylabeWords infoSentence={sylabelWord1}/>}  />
+                <Route path='/read-2' element={<SylabeWords infoSentence={sylabelWord2}/>}  />
+                <Route path='/read-3' element={<SylabeWords infoSentence={sylabelWord3}/>}  />
+                <Route path='/read-4' element={<SylabeWords infoSentence={sylabelWord4}/>}  />
+                <Route path='/read-5' element={<SylabeWords infoSentence={sylabelWord5}/>}  />
+                <Route path='/numberGame' element={<FlippingTable idTablePlay="play1" tableData={tableDataNum.photoData} bgTable={tableDataNum.bgTable} showBack1={false} />}  />
+                <Route path='/kitchenGame' element={<FlippingTable idTablePlay="kitU1" tableData={kitchenDataTable.photoData} bgTable={kitchenDataTable.bgTable} showBack1={true} />}  />
+                <Route path='/professions' element={<FlippingTable idTablePlay="prof1" tableData={tableProfessions.photoData} bgTable={tableProfessions.bgTable} showBack1={false} />}  />
+                <Route path='/animals' element={<FlippingTable idTablePlay="anim1" tableData={tableAnimals.photoData} bgTable={tableAnimals.bgTable} showBack1={true} />}  />
+                <Route path='/tableColor_body' element={<FlippingTable idTablePlay="colorBo1" tableData={tableColor_body.photoData} bgTable={tableColor_body.bgTable} showBack1={true} />}  />
+                <Route path='/wordTable/*' element={<WordTable />}  />
 
               </Routes>
               <WordCarousel/>
